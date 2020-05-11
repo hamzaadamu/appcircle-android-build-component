29ddcb8b80d949a917cbba980cfaed2c1406be5b
hasretsariyer <hasretsariyer@gmail.com>
2020-05-11 15:00:44 +0300

    Update and rename test.rb to test.sh

---
+++ test.sh
@@ -0,0 +1,18 @@
+set -e
+set -x
+
+git clone https://github.com/appcircleio/appcircle-sample-android.git
+
+export AC_MODULE=app
+export AC_VARIANTS=debug
+export AC_OUTPUT_TYPE=apk
+
+mkdir output-dir
+export AC_OUTPUT_DIR=./output-dir
+export AC_REPOSITORY_DIR=./appcircle-sample-android
+# gradlew path  
+export AC_PROJECT_PATH=./
+export AC_ENV_FILE_PATH=./env_variables_output.json
+  
+ruby android_build.rb
+cat AC_ENV_FILE_PATH
